6f0dd6119995b1796493368afb92f7e68d23ee27
Radu Cazacu <cazacuraduic@gmail.com>
2022-05-28 21:51:00 +0300

    Parse all links from the web page

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use error_chain::error_chain;
+use select::document::Document;
+use select::predicate::Name;
+
+error_chain!{
+ 	foreign_links{
+ 		ReqError(reqwest::Error);
+	IoError(std::io::Error);
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+     let resp = reqwest::get("https://ro.wikipedia.org/wiki/Guy_de_Maupassant")
+         .await?
+         .text()
+         .await?;
+
+			Document::from(resp.as_str())
+ 				.find(Name("a"))
+ 				.filter_map(|n| n.attr("href"))
+ 				.for_each(|x| {
+					 if x.contains("http") || x.contains("https") {
+							 println!("{}",x)
+					 }
+				 });
+
+     Ok(())
+}
+
+
