4158bde9a08a235ad561fcfb63cb7095ffd4fb25
Radu Cazacu <cazacuraduic@gmail.com>
2022-05-28 21:52:45 +0300

    Format lint

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,28 @@ use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
 
-error_chain!{
- 	foreign_links{
- 		ReqError(reqwest::Error);
-	IoError(std::io::Error);
+error_chain! {
+     foreign_links{
+         ReqError(reqwest::Error);
+    IoError(std::io::Error);
   }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let resp = reqwest::get("https://ro.wikipedia.org/wiki/Guy_de_Maupassant")
+        .await?
+        .text()
+        .await?;
 
-     let resp = reqwest::get("https://ro.wikipedia.org/wiki/Guy_de_Maupassant")
-         .await?
-         .text()
-         .await?;
+    Document::from(resp.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| {
+            if x.contains("http") || x.contains("https") {
+                println!("{}", x)
+            }
+        });
 
-			Document::from(resp.as_str())
- 				.find(Name("a"))
- 				.filter_map(|n| n.attr("href"))
- 				.for_each(|x| {
-					 if x.contains("http") || x.contains("https") {
-							 println!("{}",x)
-					 }
-				 });
-
-     Ok(())
+    Ok(())
 }
-
-
